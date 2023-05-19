6a75407e44e4ae87f47f4e03df0fa9b8cdc8fdb2
FedeCasper <federicorouyere@gmail.com>
2023-05-18 21:24:53 -0300

    Including vue in main

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
-// import { array } from "./module/functions"
-import { imprimirNumeroCards, renderizarCards, renderizaChechboxs, filtraPorInputText, filtraPorCheckbox, filtraCruzado, printMostPopular, changeStar } from "./module/functions.js";
+const {createApp} = Vue
 
-const url = "https://mindhub-xj03.onrender.com/api/amazing"
-let contenedorCards = document.getElementById('contenedorCards');
-let contadorCards = document.getElementById('contadorCards')
-let leftList = document.querySelector('.top3')
-let rightList = document.querySelector('.top6')
+const app = createApp({
 
+     data(){
+          return{
+               eventsArray: [],
+               categoryArrayNoRepeat: [],
+               selectedCategories: [],
+               checkboxFiltredArray: []
+          }
+     },
 
+     created(){
+          fetch("https://mindhub-xj03.onrender.com/api/amazing")
+          .then(response => response.json())
+          .then(data => {
+               this.eventsArray = data.events;
+               console.log(this.eventsArray);
+               this.categoryArrayNoRepeat = [...new Set(this.eventsArray.map(elemento => elemento.category))]
+               console.log(this.categoryArrayNoRepeat);
+          })
+          .catch(error => console.error(error))
+     },
 
-fetch(url)
-.then(response => response.json())
-.then(data => {
-     let arrayEventos = data.events
-     renderizarCards(arrayEventos, contenedorCards)
-     let contenedorCheckboxs = document.getElementById('contenedorCheckboxs');
-     renderizaChechboxs(arrayEventos, contenedorCheckboxs )
-     let botonBuscar = document.getElementById('botonBuscar')
-     let inputText = document.getElementById('inputText')
-     let checkboxs = document.querySelectorAll('input[type="checkbox"]')
-     let star = document.querySelectorAll('.star')
-     botonBuscar.addEventListener('click', () => filtraCruzado(checkboxs, arrayEventos, inputText))
-     checkboxs.forEach(checkbox => checkbox.addEventListener('change', () => filtraCruzado(checkboxs, arrayEventos, inputText)))
-     printMostPopular(arrayEventos, leftList, rightList)
-     star.forEach( element => element.addEventListener( 'click', (e) => changeStar(e, element) ) )
+     methods:{
+          filterByCheckbox(){
+               this.checkboxFiltredArray = this.eventsArray.filter( e => this.selectedCategories.includes(e.category))
+               console.log(this.checkboxFiltredArray);
+          }
+     }
 
 })
-.catch(error => console.error(error))
+
+app.mount('#app')
+
+
+
+
 
 
